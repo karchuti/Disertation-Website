eb3620685b5211bf21336d8431a5f4a8bac99f19
karchuti <karchuti@gmail.com>
2023-06-07 18:30:49 +0100

    added next and previous buttons

--- assets/js/Disertation.js
+++ assets/js/Disertation.js
@@ -25,14 +25,15 @@ searchButton.addEventListener("click", function () {
   }
 
   // Run the clear function to remove previous highlights
-  clearButton.click();
+  clearSearchResults();
 
   for (var i = 0; i < elementsToSearch.length; i++) {
     var element = elementsToSearch[i];
     var text = element.innerText;
 
     if (text.includes(input)) {
-      var highlightedText = text.replace(new RegExp(input, "gi"), function (match) {
+      var regex = new RegExp("(\\b" + input + "\\b)", "gi");
+      var highlightedText = text.replace(regex, function (match) {
         count++; // Increment the match count for each match
         return "<span class='highlight'>" + match + "</span>";
       });
@@ -52,7 +53,7 @@ searchButton.addEventListener("click", function () {
     clearButton.classList.remove("hidden"); // Show the clear button
     searchButton.classList.add("hidden"); // Hide the search button
     currentIndex = -1; // Reset the current index
-    scrollToNextMatch(); // Scroll to the first match
+    scrollToNextMatch(); // Scroll to the first match and highlight it
   } else {
     alert("No matches found.");
   }
@@ -61,30 +62,52 @@ searchButton.addEventListener("click", function () {
   document.getElementById("search-input").value = input;
 });
 
-// ...
+clearButton.addEventListener("click", clearSearchResults);
 
 previousButton.addEventListener("click", function () {
   if (currentIndex > 0) {
-    // Remove blue background color from the current highlighted element
-    highlightedElements[currentIndex].style.backgroundColor = "";
-
+    // Remove the current highlight from the previous highlighted element
+    highlightedElements[currentIndex].querySelector(".highlight").classList.remove("current-highlight");
     currentIndex--;
+    // Add the current highlight to the new current highlighted element
+    highlightedElements[currentIndex].querySelector(".highlight").classList.add("current-highlight");
     scrollToElement(highlightedElements[currentIndex]);
   }
 });
 
 nextButton.addEventListener("click", function () {
   if (currentIndex < highlightedElements.length - 1) {
-    // Remove blue background color from the current highlighted element
-    highlightedElements[currentIndex].style.backgroundColor = "";
-
+    // Remove the current highlight from the previous highlighted element
+    highlightedElements[currentIndex].querySelector(".highlight").classList.remove("current-highlight");
     currentIndex++;
+    // Add the current highlight to the new current highlighted element
+    highlightedElements[currentIndex].querySelector(".highlight").classList.add("current-highlight");
     scrollToElement(highlightedElements[currentIndex]);
   }
 });
 
+function clearSearchResults() {
+  var elementsToClear = document.querySelectorAll(".highlight");
+
+  for (var i = 0; i < elementsToClear.length; i++) {
+    var element = elementsToClear[i];
+    element.outerHTML = element.innerText;
+  }
+
+  document.getElementById("search-input").value = "";
+  clearButton.classList.add("hidden"); // Hide the clear button
+  matchCount.classList.add("hidden"); // Hide the match count
+  previousButton.classList.add("hidden"); // Hide the previous match button
+  nextButton.classList.add("hidden"); // Hide the next match button
+  searchButton.classList.remove("hidden"); // Show the search button
+  highlightedElements = []; // Clear the highlighted elements array
+  currentIndex = -1; // Reset the current index
+}
+
 function scrollToNextMatch() {
   currentIndex = 0; // Set the current index to the first match
+  // Add the current highlight to the first match
+  highlightedElements[currentIndex].querySelector(".highlight").classList.add("current-highlight");
   scrollToElement(highlightedElements[currentIndex]);
 }
 
